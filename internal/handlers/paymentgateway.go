2959b8d75bcf4df8a225660f3475ff5e1c1b5ca1
miklisanton <miklis2003@mail.ru>
2025-02-20 13:27:11 +0300

    testing links

--- internal/handlers/paymentgateway.go
+++ internal/handlers/paymentgateway.go
@@ -122,14 +122,15 @@ func (contr *Controller) CreateOrder(c echo.Context) error {
 		"uuid":     respStruct.Result.Uuid,
 	}).Msg("Invoice created")
 
-	return c.JSON(http.StatusOK, map[string]any{
-		"message": "Invoice created",
-		"invoice": map[string]any{
-			"url":      respStruct.Result.Url,
-			"order_id": respStruct.Result.OrderID,
-			"uuid":     respStruct.Result.Uuid,
-		},
-	})
+	return c.Redirect(http.StatusSeeOther, respStruct.Result.Url)
+	//	return c.JSON(http.StatusOK, map[string]any{
+	//		"message": "Invoice created",
+	//		"invoice": map[string]any{
+	//			"url":      respStruct.Result.Url,
+	//			"order_id": respStruct.Result.OrderID,
+	//			"uuid":     respStruct.Result.Uuid,
+	//		},
+	//	})
 }
 
 // ConfirmOrder is a webhook endpoint for cryptomus gateway
